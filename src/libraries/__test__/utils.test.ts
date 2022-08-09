462fd4617a47b10258561e64c6383bdc48f1c217
Leo Wu <lwu@a10networks.com>
2022-08-09 20:40:51 +0800

    test: add tests

--- src/libraries/__test__/utils.test.ts
+++ src/libraries/__test__/utils.test.ts
@@ -2,19 +2,47 @@ import { passwordRestrictions, validatePassword } from '../utils'
 
 describe('utils', () => {
   describe('passwordRestrictions validator should return expected result', () => {
+    const MIN_OF_CHAR_CODE = 0
+    const MAX_OF_CHAR_CODE = 65535
+
     test('when input has upper case letter', () => {
       const upperCaseValidator = passwordRestrictions[0].validator
-      expect(upperCaseValidator('L')).toBeTruthy()
+
+      const CHAR_CODE_OF_UPPER_A = 'A'.charCodeAt(0)
+      const CHAR_CODE_OF_UPPER_Z = 'Z'.charCodeAt(0)
+      for (let i = MIN_OF_CHAR_CODE, maxI = MAX_OF_CHAR_CODE; i <= maxI; i++) {
+        if (CHAR_CODE_OF_UPPER_A <= i && i <= CHAR_CODE_OF_UPPER_Z) {
+          expect(upperCaseValidator(String.fromCharCode(i))).toBeTruthy()
+        } else {
+          expect(upperCaseValidator(String.fromCharCode(i))).toBeFalsy()
+        }
+      }
     })
 
     test('when input has lower case letter', () => {
       const lowerCaseValidator = passwordRestrictions[1].validator
-      expect(lowerCaseValidator('l')).toBeTruthy()
+      const CHAR_CODE_OF_LOWER_A = 'a'.charCodeAt(0)
+      const CHAR_CODE_OF_LOWER_Z = 'z'.charCodeAt(0)
+      for (let i = MIN_OF_CHAR_CODE, maxI = MAX_OF_CHAR_CODE; i <= maxI; i++) {
+        if (CHAR_CODE_OF_LOWER_A <= i && i <= CHAR_CODE_OF_LOWER_Z) {
+          expect(lowerCaseValidator(String.fromCharCode(i))).toBeTruthy()
+        } else {
+          expect(lowerCaseValidator(String.fromCharCode(i))).toBeFalsy()
+        }
+      }
     })
 
     test('when input has number', () => {
       const numberValidator = passwordRestrictions[2].validator
-      expect(numberValidator('1')).toBeTruthy()
+      const CHAR_CODE_OF_LOWER_ZERO = '0'.charCodeAt(0)
+      const CHAR_CODE_OF_LOWER_NINE = '9'.charCodeAt(0)
+      for (let i = MIN_OF_CHAR_CODE, maxI = MAX_OF_CHAR_CODE; i <= maxI; i++) {
+        if (CHAR_CODE_OF_LOWER_ZERO <= i && i <= CHAR_CODE_OF_LOWER_NINE) {
+          expect(numberValidator(String.fromCharCode(i))).toBeTruthy()
+        } else {
+          expect(numberValidator(String.fromCharCode(i))).toBeFalsy()
+        }
+      }
     })
 
     test('when input has special character', () => {
