04c6c7d640e213abcfdcb3de0c77e2b2292ed780
Leo Wu <lwu@a10networks.com>
2022-08-10 15:14:34 +0800

    style: stick to mockup

--- src/components/BirthdayPicker/styles.ts
+++ src/components/BirthdayPicker/styles.ts
@@ -1,7 +1,30 @@
 import { css } from '@emotion/react'
 
+import { fontFamily, fontSize, fontWeight } from 'src/GlobalCss'
+
 export const birthdayPicker = css`
   * {
-    font-family: 'ubuntu' !important;
+    font-family: ${fontFamily.ubuntu} !important;
+  }
+
+  width: 335px;
+  height: 58px;
+  box-sizing: border-box;
+
+  .MuiInputBase-root {
+    height: 58px;
+
+    input {
+      height: 24px;
+      padding: 0 0 0 12px;
+      font-size: ${fontSize.large};
+      font-weight: ${fontWeight.lighter};
+      line-height: 24px;
+      letter-spacing: 0.15px;
+    }
+
+    legend {
+      width: 57px;
+    }
   }
 `
